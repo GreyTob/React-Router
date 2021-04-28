6532c6f9a456ef174d7acb873e765467ffbac06f
Sergey <GreyTob@inbox.ru>
2021-04-28 19:30:16 +0500

    routing and components

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ class App extends Component {
             <li>
               <a href="/about">About</a>
             </li>
+            <li>
+              <a href="/cars">Cars</a>
+            </li>
           </ul>
         </nav>
 
@@ -24,9 +27,9 @@ class App extends Component {
         {/*localhost:3000*/}
         <Route path="/" exact render={()=> <h1>Home Page from Route</h1>} />
 
-        <About />
+        <Route path="/about" component={About} />
+        <Route path="/cars" component={Cars} />
 
-        <Cars />
       </div>
     );
   }
